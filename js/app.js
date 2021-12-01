57663805fe3959220fcc45a029502ba81ffd5bcd
Shenja <shenja.chekan@gmail.com>
2021-12-01 12:51:41 +0300

    добавлена валидация поля при введении имени, и JSON файл перенесен в отдельный файл и помещена в корень приложения

--- js/app.js
+++ js/app.js
@@ -1,73 +1,3 @@
-const surveyJSON = `[{
-    "nameStartButton": "Старт",
-    "nameNextButton": "Далее",
-    "nameFinishButton": "Показать результаты",
-    "questions": [
-        {
-            "id": 1,
-            "quest": "How old are you?",
-            "answerType": "radio",
-            "answers": [
-                {
-                    "id": 5432,
-                    "answer": "< 18"
-                }, {
-                    "id": 3428,
-                    "answer": "> 18 and < 25"
-                }, {
-                    "id": 6754,
-                    "answer": "> 25 and < 43"
-                }, {
-                    "id": 1953,
-                    "answer": "> 43"
-                }
-            ]
-        },
-        {
-            "id": 2,
-            "quest": "What color are the eyes?",
-            "answerType": "radio",
-            "answers": [
-                {
-                    "id": 7863,
-                    "answer": "Blue"
-                }, {
-                    "id": 5643,
-                    "answer": "Green"
-                }, {
-                    "id": 6754,
-                    "answer": "Hazel"
-                }, {
-                    "id": 8789,
-                    "answer": "Red"
-                }
-            ]
-        },
-        {
-            "id": 3,
-            "quest": "Your education?",
-            "answerType": "checkbox",
-            "answers": [
-                {
-                    "id": 7863,
-                    "answer": "Specialized secondary education"
-                }, {
-                    "id": 5643,
-                    "answer": "Higher education"
-                }, {
-                    "id": 6754,
-                    "answer": "General education"
-                }
-            ]
-        },
-        {
-            "id": 4,
-            "quest": "Your city of residence?",
-            "answerType": "text"
-        }
-    ]
-}]`;
-
 class FormSurveyUser {
     constructor(name) {
         this._name = name;
@@ -79,7 +9,7 @@ class FormSurveyUser {
     }
 };
 
-const Survey = JSON.parse(surveyJSON)[0];
+const Survey = JSON.parse(dataServer)[0];
 
 const surveyStart = document.querySelector(".survey__start");
 const btnStart = surveyStart.querySelector(".btn__start");
